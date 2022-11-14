f18b70698fac50ac0fb5bc6938ea14b36f04d2d1
vinicius.alkimin <viniciusg_alkimin@hotmail.com>
2022-11-14 18:05:53 -0300

    Project created

---
+++ src/test/java/com/alkimin/springMongoDB/SpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkimin.springMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
